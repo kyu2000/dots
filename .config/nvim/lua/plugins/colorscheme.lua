0922318c0a0cb863d4404c7e49919c016b246f83
Kenvin Yu <kenvinyu2000@gmail.com>
2023-07-12 22:18:28 +1000

    Create colorscheme.lua

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+    {
+        "marko-cerovac/material.nvim", 
+        lazy = false,
+        opts = {
+            plugins = {
+                "telescope",
+                "which-key"
+            },
+            high_visibility = {
+                lighter = true,
+                darker = true
+            }
+        }
+    }
+}
+
