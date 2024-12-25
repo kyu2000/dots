6d1181b9f79f78080a9e68423648c127a53ac4b6
Kenvin Yu <kenvinyu2000@gmail.com>
2024-12-25 13:09:36 +1100

    nvim: Update which-key config
    This change:
    - Fixes format for lua config file
    - Adds which-key groups for "[" and "]"
    - Add key mapping for showing buffer local keymaps

---
+++ .config/nvim/lua/plugins/whichkey.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      spec = {
+        mode = { "n", "v" },
+        { "[",          group = "prev" },
+        { "]",          group = "next" },
+      },
+    },
+    keys = {
+      {
+        "<leader>?",
+        function()
+          require("which-key").show({ global = false })
+        end,
+        desc = "Buffer Local Keymaps (which-key)",
+      },
+    },
+  },
+}
