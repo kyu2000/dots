1ad30d5da23a313f5163ace9894917bd35198656
Kenvin Yu <kenvinyu2000@gmail.com>
2024-12-25 13:18:27 +1100

    nvim: Add lazydev for nvim lsp

---
+++ .config/nvim/lua/plugins/lazydev.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "folke/lazydev.nvim",
+    ft = "lua",
+    opts = {
+      library = {
+        { path = "luvit-meta/library", words = { "vim%.uv" } },
+      },
+    },
+  },
+  {
+    "Bilal2453/luvit-meta",
+    lazy = true,
+  },
+  {
+    "saghen/blink.cmp",
+    opts = {
+      sources = {
+        default = { "lazydev" },
+        providers = {
+          lazydev = {
+            name = "LazyDev",
+            module = "lazydev.integrations.blink",
+            score_offset = 100,
+          },
+        },
+      },
+    },
+  },
+}
