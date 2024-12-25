d1059f20972699518c8e35c099e817603ae93d9f
Kenvin Yu <kenvinyu2000@gmail.com>
2024-12-25 16:14:55 +1100

    nvim: Consolidate lsp keymaps and config with plugin spec

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,25 +1,49 @@
 return {
-    {
-        "neovim/nvim-lspconfig",
-        dependencies = {
-            "williamboman/mason.nvim",
-            {
-                "williamboman/mason-lspconfig.nvim",
-                opts = {
-                    ensure_installed = {
-                        "jdtls",
-                        "lua_ls",
-                        "jedi_language_server",
-                    },
-                },
-            },
-        },
+  {
+    "williamboman/mason.nvim",
+    lazy = true,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = true,
+    opts = {
+      ensure_installed = {
+        "lua_ls",
+        "taplo",
+      },
     },
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-cmdline",
-    "hrsh7th/nvim-cmp",
-    "hrsh7th/cmp-vsnip",
-    "hrsh7th/vim-vsnip",
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "mason-lspconfig.nvim",
+    },
+    event = "VeryLazy",
+    config = function()
+      require("mason").setup()
+      require("mason-lspconfig").setup()
+
+      local on_attach = function(client, bufnr)
+        vim.api.nvim_set_option_value('omnifunc', 'v:lua.vim.lsp.omnifunc', { buf = bufnr })
+        local function map(mode, lhs, rhs, desc)
+          vim.keymap.set(mode, lhs, rhs, { desc = desc, noremap = true, silent = true, buffer = bufnr })
+        end
+        map("n", "gd", "<cmd>Telescope lsp_definitions<cr>", "LSP definition")
+        map("n", "gD", "<cmd>Telescope lsp_type_definitions<cr>", "LSP type definition")
+        map("n", "gr", "<cmd>Telescope lsp_references<cr>", "LSP references")
+        map("n", "gi", "<cmd>Telescope lsp_implementations<cr>", "LSP implementations")
+        map("n", "gh", "<cmd>lua vim.lsp.buf.signature_help()<CR>", "LSP signature help")
+        map("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", "LSP hover")
+        map("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", "LSP previous diagnostic")
+        map("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", "LSP next diagnostic")
+      end
+
+      require("lspconfig").lua_ls.setup({
+        on_attach = on_attach,
+      })
+      require("lspconfig").taplo.setup({
+        on_attach = on_attach,
+      })
+    end,
+  },
 }
