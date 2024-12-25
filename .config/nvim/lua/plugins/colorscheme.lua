7ddb3795cc5b4030b2ac8f5bec5b9775eb0b4e7a
Kenvin Yu <kenvinyu2000@gmail.com>
2024-12-25 14:51:27 +1100

    nvim: Add theme integrations for lsp

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -18,6 +18,27 @@ return {
           scope_color = "text",
           colored_indent_levels = false,
         },
+        mason = true,
+        native_lsp = {
+          enabled = true,
+          virtual_text = {
+            errors = { "italic" },
+            hints = { "italic" },
+            warnings = { "italic" },
+            information = { "italic" },
+            ok = { "italic" },
+          },
+          underlines = {
+            errors = { "underline" },
+            hints = { "underline" },
+            warnings = { "underline" },
+            information = { "underline" },
+            ok = { "underline" },
+          },
+          inlay_hints = {
+            background = true,
+          },
+        },
         neotree = true,
         noice = true,
       },
