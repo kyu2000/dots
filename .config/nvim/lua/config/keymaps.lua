e68228b275fa80fd8b6d6f520ab5e1c94a4bf598
Kenvin Yu <kenvinyu2000@gmail.com>
2024-12-25 16:43:35 +1100

    nvim: Add more system clipboard keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,2 +1,4 @@
--- system clipboard
-vim.keymap.set({"n", "v"}, "<leader>y", "\"+y", {desc = "Yank to system clipboard", noremap = true})
+-- System clipboard
+vim.keymap.set({ "n", "v" }, "<leader>y", "\"+y", { desc = "Yank to system clipboard", noremap = true })
+vim.keymap.set({ "n", "v" }, "<leader>p", "\"+p", { desc = "Paste from system clipboard after cursor", noremap = true })
+vim.keymap.set({ "n", "v" }, "<leader>P", "\"+P", { desc = "Paste from system clipboard before cursor", noremap = true })
