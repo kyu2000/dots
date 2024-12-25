6048ba6c89f4cedacc2ba17bd71ca1e50ef39e90
Kenvin Yu <kenvinyu2000@gmail.com>
2024-12-25 16:44:06 +1100

    nvim: Add keymaps for resizing window

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,3 +2,9 @@
 vim.keymap.set({ "n", "v" }, "<leader>y", "\"+y", { desc = "Yank to system clipboard", noremap = true })
 vim.keymap.set({ "n", "v" }, "<leader>p", "\"+p", { desc = "Paste from system clipboard after cursor", noremap = true })
 vim.keymap.set({ "n", "v" }, "<leader>P", "\"+P", { desc = "Paste from system clipboard before cursor", noremap = true })
+
+-- Resize window using <ctrl> arrow keys
+vim.keymap.set("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+vim.keymap.set("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
