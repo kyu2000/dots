398ebd40ce9e21e2d414ddc92210288bc2984279
Kenvin Yu <kenvinyu2000@gmail.com>
2025-02-08 15:38:41 +1100

    nvim: Remove telescope-live-grep-args

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -3,12 +3,12 @@ local telescope_resume = function()
 end
 
 local telescope_find_files = function()
-  return require("telescope.builtin").find_files({ hidden = true })
+  return require("telescope.builtin").find_files()
 end
 
 local telescope_find_files_relative_to_buffer = function()
   local utils = require("telescope.utils")
-  return require("telescope.builtin").find_files({ cwd = utils.buffer_dir(), hidden = true })
+  return require("telescope.builtin").find_files({ cwd = utils.buffer_dir() })
 end
 
 local telescope_old_files = function()
@@ -16,11 +16,11 @@ local telescope_old_files = function()
 end
 
 local telescope_live_grep = function()
-  return require("telescope").extensions.live_grep_args.live_grep_args()
+  return require("telescope.builtin").live_grep()
 end
 
 local telescope_grep_word_under_cursor = function()
-  return require("telescope-live-grep-args.shortcuts").grep_word_under_cursor()
+  return require("telescope.builtin").grep_string()
 end
 
 local telescope_buffers = function()
@@ -48,28 +48,35 @@ return {
         "nvim-telescope/telescope-fzf-native.nvim",
         build = "make",
       },
-      {
-        "nvim-telescope/telescope-live-grep-args.nvim",
-        version = "^1.0.0",
-      },
     },
-    opts = function()
-      local live_grep_args_actions = require("telescope-live-grep-args.actions")
+    opts = function(_)
       return {
         defaults = {
           path_display = {
             "truncate",
           },
-        },
-        extensions = {
-          live_grep_args = {
-            auto_quoting = true,
-            mappings = {
-              i = {
-                ["<C-k>"] = live_grep_args_actions.quote_prompt(),
-                ["<C-i>"] = live_grep_args_actions.quote_prompt({ postfix = " --iglob " }),
-              },
+          mappings = {
+            i = {
+              ["<C-space>"] = require("telescope.actions").to_fuzzy_refine,
             },
+          },
+        },
+        pickers = {
+          find_files = {
+            file_ignore_patterns = { "node_modules", ".git", ".venv" },
+            hidden = true,
+          },
+          live_grep = {
+            file_ignore_patterns = { "node_modules", ".git", ".venv" },
+            additional_args = function(_)
+              return { "--hidden" }
+            end,
+          },
+          grep_string = {
+            file_ignore_patterns = { "node_modules", ".git", ".venv" },
+            additional_args = function(_)
+              return { "--hidden" }
+            end,
           },
         },
       }
@@ -78,19 +85,18 @@ return {
       local telescope = require("telescope")
       telescope.setup(opts)
       telescope.load_extension("fzf")
-      telescope.load_extension("live_grep_args")
     end,
     keys = {
-      { "<leader>fr", telescope_resume,                        desc = "Resume last telescope" },
-      { "<leader>ff", telescope_find_files,                    desc = "Find files" },
+      { "<leader>fr", telescope_resume, desc = "Resume last telescope" },
+      { "<leader>ff", telescope_find_files, desc = "Find files" },
       { "<leader>fd", telescope_find_files_relative_to_buffer, desc = "Find files relative to buffer" },
-      { "<leader>fo", telescope_old_files,                     desc = "Recent files" },
-      { "<leader>fg", telescope_live_grep,                     desc = "Live grep" },
-      { "<leader>fw", telescope_grep_word_under_cursor,        desc = "Grep word under cursor" },
-      { "<leader>fb", telescope_buffers,                       desc = "Buffers" },
-      { "<leader>fc", telescope_command_history,               desc = "Command history" },
-      { "<leader>fs", telescope_search_history,                desc = "Search history" },
-      { "<leader>fq", telescope_quickfix_list,                 desc = "Quickfix list" },
+      { "<leader>fo", telescope_old_files, desc = "Recent files" },
+      { "<leader>fg", telescope_live_grep, desc = "Live grep" },
+      { "<leader>fw", telescope_grep_word_under_cursor, desc = "Grep word under cursor" },
+      { "<leader>fb", telescope_buffers, desc = "Buffers" },
+      { "<leader>fc", telescope_command_history, desc = "Command history" },
+      { "<leader>fs", telescope_search_history, desc = "Search history" },
+      { "<leader>fq", telescope_quickfix_list, desc = "Quickfix list" },
     },
     cmd = "Telescope",
   },
