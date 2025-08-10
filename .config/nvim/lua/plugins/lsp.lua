49c98586af444fb744c55037205a5f141bb9ca20
Kenvin Yu <kenvinyu2000@gmail.com>
2025-08-10 22:08:15 +1000

    nvim: Update lsp config for 0.11

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,51 +1,65 @@
-local function lsp_on_attach(client, bufnr)
-  vim.api.nvim_set_option_value("omnifunc", "v:lua.vim.lsp.omnifunc", { buf = bufnr })
-  local function map(mode, lhs, rhs, desc)
-    vim.keymap.set(mode, lhs, rhs, { desc = desc, noremap = true, silent = true, buffer = bufnr })
-  end
-  map("n", "gd", "<cmd>Telescope lsp_definitions<cr>", "LSP definition")
-  map("n", "gD", "<cmd>Telescope lsp_type_definitions<cr>", "LSP type definition")
-  map("n", "gr", "<cmd>Telescope lsp_references<cr>", "LSP references")
-  map("n", "gi", "<cmd>Telescope lsp_implementations<cr>", "LSP implementations")
-  map("n", "gh", "<cmd>lua vim.lsp.buf.signature_help()<CR>", "LSP signature help")
-  map("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", "LSP hover")
-  map("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", "LSP previous diagnostic")
-  map("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", "LSP next diagnostic")
-end
-
-local function default_handler(server_name)
-  require("lspconfig")[server_name].setup({
-    capabilities = require("blink.cmp").get_lsp_capabilities(),
-    on_attach = lsp_on_attach,
-  })
-end
-
 return {
   {
     "williamboman/mason.nvim",
-    lazy = true,
+    opts = {},
   },
   {
     "williamboman/mason-lspconfig.nvim",
-    lazy = true,
+    dependencies = {
+      "williamboman/mason.nvim",
+      "neovim/nvim-lspconfig",
+    },
     opts = {
       ensure_installed = {
         "lua_ls",
-      },
-      handlers = {
-        default_handler,
       },
     },
   },
   {
     "neovim/nvim-lspconfig",
-    dependencies = {
-      "mason-lspconfig.nvim",
-    },
-    event = "VeryLazy",
     config = function()
-      require("mason").setup()
-      require("mason-lspconfig").setup()
+      vim.lsp.config("*", {
+        capabilities = {
+          textDocument = {
+            semanticTokens = {
+              multilineTokenSupport = true,
+            },
+          },
+        },
+        root_markers = { ".git" },
+      })
+
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+          local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+
+          local function map(mode, lhs, rhs, desc)
+            vim.keymap.set(mode, lhs, rhs, { desc = desc, noremap = true, silent = true, buffer = args.buf })
+          end
+
+          if client:supports_method("textDocument/definition") then
+            map("n", "gd", "<cmd>Telescope lsp_definitions<cr>", "LSP definition")
+          end
+          if client:supports_method("textDocument/typeDefinition") then
+            map("n", "gD", "<cmd>Telescope lsp_type_definitions<cr>", "LSP type definition")
+          end
+          if client:supports_method("textDocument/implementation") then
+            map("n", "gi", "<cmd>Telescope lsp_implementations<cr>", "LSP implementations")
+          end
+          if client:supports_method("textDocument/references") then
+            map("n", "gr", "<cmd>Telescope lsp_references<cr>", "LSP references")
+          end
+          if client:supports_method("textDocument/signatureHelp") then
+            map("n", "gh", "<cmd>lua vim.lsp.buf.signature_help()<CR>", "LSP signature help")
+          end
+          if client:supports_method("textDocument/hover") then
+            map("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", "LSP hover")
+          end
+          if client:supports_method("textDocument/switchSourceHeader") then
+            map("n", "gs", "<cmd>LspClangdSwitchSourceHeader<CR>", "LSP switch source header")
+          end
+        end,
+      })
     end,
   },
 }
