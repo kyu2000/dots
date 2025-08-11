fe7afe63785cf11323d8c51d88fe817c33eb0c2c
Kenvin Yu <kenvinyu2000@gmail.com>
2025-08-11 22:10:52 +1000

    nvim: Enable snacks words for auto-highlighting

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -14,6 +14,25 @@ return {
           enabled = false,
         },
       },
+      words = { enabled = true },
+    },
+    keys = {
+      {
+        "]]",
+        function()
+          Snacks.words.jump(vim.v.count1)
+        end,
+        desc = "Next Reference",
+        mode = { "n", "t" },
+      },
+      {
+        "[[",
+        function()
+          Snacks.words.jump(-vim.v.count1)
+        end,
+        desc = "Prev Reference",
+        mode = { "n", "t" },
+      },
     },
     init = function()
       vim.api.nvim_create_autocmd("User", {
