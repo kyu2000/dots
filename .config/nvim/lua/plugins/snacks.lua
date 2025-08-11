443f6910376d3e4b53701f49773f4ef3b389a820
Kenvin Yu <kenvinyu2000@gmail.com>
2025-08-11 22:05:56 +1000

    nvim: Replace ibl with snacks indent

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -5,6 +5,15 @@ return {
     lazy = false,
     opts = {
       dashboard = { enabled = true },
+      indent = {
+        enabled = true,
+        indent = {
+          char = "▏",
+        },
+        scope = {
+          enabled = false,
+        },
+      },
     },
     init = function()
       vim.api.nvim_create_autocmd("User", {
@@ -14,7 +23,7 @@ return {
           require("snacks").toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
           require("snacks").toggle.line_number():map("<leader>ul")
           require("snacks").toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
-        end
+        end,
       })
     end,
   },
