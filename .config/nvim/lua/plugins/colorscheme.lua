36bee5fab29342a3e872fca17c1eb9e5a9f82379
Kenvin Yu <kenvinyu2000@gmail.com>
2025-08-11 21:57:18 +1000

    nvim: Use auto_integrations in catppuccin colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -5,44 +5,7 @@ return {
     lazy = false,
     priority = 1000,
     opts = {
-      integrations = {
-        barbecue = {
-          dim_dirname = true,
-          bold_basename = true,
-          dim_context = false,
-          alt_background = false,
-        },
-        blink_cmp = true,
-        gitsigns = true,
-        indent_blankline = {
-          enabled = true,
-          scope_color = "text",
-          colored_indent_levels = false,
-        },
-        mason = true,
-        native_lsp = {
-          enabled = true,
-          virtual_text = {
-            errors = { "italic" },
-            hints = { "italic" },
-            warnings = { "italic" },
-            information = { "italic" },
-            ok = { "italic" },
-          },
-          underlines = {
-            errors = { "underline" },
-            hints = { "underline" },
-            warnings = { "underline" },
-            information = { "underline" },
-            ok = { "underline" },
-          },
-          inlay_hints = {
-            background = true,
-          },
-        },
-        neotree = true,
-        noice = true,
-      },
+      auto_integrations = true,
     },
     config = function(_, opts)
       require("catppuccin").setup(opts)
