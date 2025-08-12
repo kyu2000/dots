227749a84ef090877a98cb671d737a9c0b7f683d
Kenvin Yu <kenvinyu2000@gmail.com>
2025-08-12 21:03:28 +1000

    nvim: Enable snacks picker with keymaps

--- .config/nvim/lua/plugins/whichkey.lua
+++ .config/nvim/lua/plugins/whichkey.lua
@@ -5,9 +5,10 @@ return {
     opts = {
       spec = {
         mode = { "n", "v" },
-        { "<leader>f",  group = "file/find" },
+        { "<leader>f",  group = "find" },
         { "<leader>g",  group = "git" },
         { "<leader>gh", group = "hunks" },
+        { "<leader>s",  group = "search" },
         { "<leader>n",  group = "noice" },
         { "<leader>q",  group = "quit/session" },
         { "<leader>u",  group = "setting toggles" },
