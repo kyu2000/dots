550520b0959abfcdb1855bd1fe33e46f36cde3ed
Kenvin Yu <kenvin.yu@imc.com>
2025-08-16 13:20:42 +1000

    nvim: Create keymap for switching between source and header

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -28,6 +28,19 @@ return {
         },
         root_markers = { ".git" },
       })
+
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+          local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+          if client:supports_method("textDocument/switchSourceHeader") then
+            vim.keymap.set("n", "gs", "<cmd>LspClangdSwitchSourceHeader<CR>", {
+              desc = "LSP switch source header",
+              silent = true,
+              buffer = args.buf,
+            })
+          end
+        end,
+      })
     end,
   },
 }
